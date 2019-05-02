6b39b5f314aa39309498b37c689d5611cef23304
Richard Buchanan (PhD Immuno Rjb590 Immunot FT (A300)) <RJB590@student.bham.ac.uk>
2019-05-02 16:07:13 +0100

    Second 4.1 commit. Basic changes.

--- 4.1_functional_enrichment/4_1_functional_enrichment_script.R
+++ 4.1_functional_enrichment/4_1_functional_enrichment_script.R
@@ -9,3 +9,22 @@ BiocManager::install("GSVAdata")
 library(GSVAdata)
 setwd("L:/Richard B/Analysis/2019/May 2019/testing_the_GSVA_package/Input")
 
+# Load C2BroadSets
+## This is a GeneSetCollection object (gmt)
+### Can use the getGmt("x") command to do this from gmt formatted text files, like 'test_enrich_Set_gmt_idea.txt' from the 'Input'
+data("c2BroadSets")
+View(c2BroadSets)
+
+
+# Load Leukemia data
+## This is very different data because is in a ExpressionSet object 
+data(leukemia)
+View(leukemia_eset)
+
+# View the subtypes
+head(pData(leukemia_eset))
+table(leukemia_eset$subtype)
+
+
+
+
