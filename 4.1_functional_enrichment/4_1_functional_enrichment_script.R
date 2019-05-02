a0110c8b5ab13525721dfd577974a03a08669b39
Richard Buchanan (PhD Immuno Rjb590 Immunot FT (A300)) <RJB590@student.bham.ac.uk>
2019-05-02 17:53:36 +0100

    Functional Enrichment Script of Leukemia initial commit.

--- 4.1_functional_enrichment/4_1_functional_enrichment_script.R
+++ 4.1_functional_enrichment/4_1_functional_enrichment_script.R
@@ -1,12 +1,6 @@
 ### 4.1 GSVA Vignette Functional Enrichment Script of Leukaemia Data ###
-library(GSEABase)
-library(GSVA)
-library(Biobase, genefilter, limma)
-library(RColorBrewer)
-if (!requireNamespace("BiocManager", quietly = TRUE))
-  install.packages("BiocManager")
-BiocManager::install("GSVAdata")
-library(GSVAdata)
+mypackages <- c("GSEABase", "GSVA", "Biobase", "genefilter", "limma", "RColorBrewer", "GSVAdata")
+lapply(mypackages, library, character.only = T)
 setwd("L:/Richard B/Analysis/2019/May 2019/testing_the_GSVA_package/Input")
 
 # Load C2BroadSets
@@ -25,6 +19,15 @@ View(leukemia_eset)
 head(pData(leukemia_eset))
 table(leukemia_eset$subtype)
 
+# Clean up step
+filtered_eset <- nsFilter(leukemia_eset, require.entrez = T, remove.dupEntrez = T, var.func = IQR, 
+                          var.filter = T, var.cutoff = 0.5, filterByQuantile = T, feature.exclude = "^AFFX")
+View(filtered_eset)
 
+# Return all the information for these 4292 features
+leukemia_filtered_eset <- filtered_eset$eset
+View(leukemia_filtered_eset)
 
+# GSVA
+leukemia_es <- gsva(leukemia_filtered_eset, c2BroadSets, min.sz = 10, max.sz = 500, verbose = T)
 
