7455bb456a7cbc721d536db3b39903af1071cc41
Richard Buchanan (PhD Immuno Rjb590 Immunot FT (A300)) <RJB590@student.bham.ac.uk>
2019-05-03 09:14:36 +0100

    Finished 4.1 Functional Enrichment Script.

--- 4.1_functional_enrichment/4_1_functional_enrichment_script.R
+++ 4.1_functional_enrichment/4_1_functional_enrichment_script.R
@@ -31,3 +31,25 @@ View(leukemia_filtered_eset)
 # GSVA
 leukemia_es <- gsva(leukemia_filtered_eset, c2BroadSets, min.sz = 10, max.sz = 500, verbose = T)
 
+# Statistical Comparison
+## Script below the break shows that are 34 differentially activated C2 pathways between MLL and ALL
+### Second break script shows the 122 differentially expressed genes with min log fold change of 2 at 0.1 FDR
+adjPvalueCutoff <- 0.001
+logFCcutoff <- log2(2)
+
+design <- model.matrix(~ factor(leukemia_es$subtype))
+colnames(design) <- c("ALL", "MLLvsALL")
+fit <- lmFit(leukemia_es, design)
+fit2 <- eBayes(fit)
+allGeneSets <- topTable(fit2, coef = "MLLvsALL", number = Inf)
+DEgeneSets <- topTable(fit2, coef = "MLLvsALL", number = Inf, p.value = adjPvalueCutoff, adjust = "BH")
+res <- decideTests(fit2, p.value = adjPvalueCutoff)
+summary(res)
+
+fit3 <- lmFit(leukemia_filtered_eset, design)
+fit4 <- eBayes(fit3)
+allGenes <- topTable(fit4, coef = "MLLvsALL", number = Inf)
+DEgenes <- topTable(fit4, coef = "MLLvsALL", number = Inf, p.value = adjPvalueCutoff, adjust = "BH",
+                    lfc = logFCcutoff)
+res2 <- decideTests(fit4, p.value = adjPvalueCutoff, lfc = logFCcutoff)
+summary(res2)
